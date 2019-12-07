fe28fbd570f67f887403a622c4be879dbde9a804
SherifGhoz <ghozdev@gmail.com>
2019-12-07 20:21:36 +0200

    Adding Vue-Axe
    Using WCAG 2.1 AA

---
+++ plugins/axe.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import axe from 'axe-core'
+
+if (process.env.NODE_ENV !== 'production') {
+  const VueAxe = require('vue-axe')
+  Vue.use(VueAxe, {
+    config: {
+      rules: axe.getRules(['wcag21aa'])
+    },
+    clearConsoleOnUpdate: false
+  })
+}
