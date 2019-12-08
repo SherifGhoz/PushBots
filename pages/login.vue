69e1c4e1d2b231ff6e6c68b6d58c18d51f278f18
SherifGhoz <ghozdev@gmail.com>
2019-12-08 08:32:52 +0200

    Add Login Page

---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row no-gutters>
+    <v-col class="d-none d-sm-flex logo-section"> </v-col>
+    <v-col> </v-col>
+  </v-row>
+</template>
+
+<style scoped lang="scss">
+.logo-section {
+  background-color: $primary-color;
+  background-image: url(../static/logo.svg);
+  background-position: 3%;
+}
+</style>
