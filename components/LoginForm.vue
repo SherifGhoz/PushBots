059116803b66abb954c04f43480c3e2fb4e9d8a6
SherifGhoz <ghozdev@gmail.com>
2019-12-10 00:36:07 +0200

    Add Login Form

---
+++ components/LoginForm.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-row>
+    <v-form ref="form" v-model="valid">
+      <v-text-field
+        v-model="email"
+        type="email"
+        prepend-inner-icon="mail_outline"
+        :rules="emailRules"
+        label="Your e-mail address"
+        required
+        validate-on-blur
+      ></v-text-field>
+
+      <v-text-field
+        v-model="password"
+        type="password"
+        prepend-inner-icon="lock_outline"
+        :rules="passwordRules"
+        label="Password"
+        required
+      ></v-text-field>
+      <div class="help-login">
+        <a href="#">Forgot your password?</a><br />
+        <a href="#">Don't have an account?</a>
+      </div>
+      <v-btn block :disabled="!valid" color="primary">
+        LOGIN
+      </v-btn>
+      <div class="ext-logins">
+        <v-btn dark tile color="#db2a2f" class="col-12 col-md-6">
+          <v-icon small left>mdi-google</v-icon>
+          Google
+        </v-btn>
+        <v-btn dark tile color="black" class="col-12 col-md-6">
+          <v-icon small left>mdi-github-circle</v-icon>
+          Github
+        </v-btn>
+      </div>
+    </v-form>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    email: '',
+    emailRules: [
+      (v) => !!v || 'E-mail is required',
+      (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+    ],
+    password: '',
+    passwordRules: [(v) => !!v || 'Password is required']
+  }),
+
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.v-form {
+  margin: auto;
+  width: 100%;
+  max-width: 375px;
+}
+
+.v-text-field input {
+  background-color: #e6f2f1;
+}
+
+.help-login {
+  margin: 10px auto 20px;
+}
+
+.help-login a {
+  font-size: 12px;
+  font-weight: 500;
+}
+
+.ext-logins {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
